f5dbc023d51878a6b6f1f2c4e2c78940f870dda1
Benjamin Taubenblatt <BenjaminMT64@gmail.com>
2016-06-08 23:10:16 -0400

    trying to display name and such to screen and get rid of previous layout

--- SamsaoTest/Application/Login/Views/SSOLoginView.swift
+++ SamsaoTest/Application/Login/Views/SSOLoginView.swift
@@ -10,33 +10,33 @@ import UIKit
 import SteviaLayout
 
 class SSOLoginView: UIView {
-
+    
     // MARK: - Properties
-
+    
     let username = UITextField()
     let password = UITextField()
     let login = UIButton()
     private weak var vc: SSOLoginViewController?
-
+    
     // MARK: - Initialization
-
+    
     convenience init(viewController: SSOLoginViewController) {
         self.init(frame: CGRect.zero)
         vc = viewController
         render()
     }
-
+    
     // MARK: - UI
-
+    
     func render() {
         backgroundColor = UIColor.whiteColor()
-
+        
         sv(
             username.placeholder("Username").style(fieldStyle),
             password.placeholder("Password").style(fieldStyle).style(passwordFieldStyle),
             login.text("LOGIN").style(buttonStyle).tap(loginTapped)
         )
-
+        
         layout(
             100,
             |-username-| ~ 80,
@@ -46,11 +46,11 @@ class SSOLoginView: UIView {
             |login| ~ 80,
             0
         )
-
+        
         username.delegate = vc
         password.delegate = vc
     }
-
+    
     func fieldStyle(field: UITextField) {
         field.borderStyle = .RoundedRect
         field.font = UIFont(name: "HelveticaNeue-Light", size: 26)
@@ -58,12 +58,12 @@ class SSOLoginView: UIView {
         field.autocorrectionType = .No
         field.returnKeyType = .Next
     }
-
+    
     func passwordFieldStyle(field: UITextField) {
         field.secureTextEntry = true
         field.returnKeyType = .Done
     }
-
+    
     func buttonStyle(button: UIButton) {
         button.backgroundColor = .lightGrayColor()
     }
@@ -71,43 +71,79 @@ class SSOLoginView: UIView {
     
     func buttonClicked(sender:UIButton){
         do{
-        login.backgroundColor = UIColor.blueColor()
-        //declare parameter as a dictionary which contains string as key and value combination.
-        let parameters = ["username": username.text!, "password": password.text!] as Dictionary<String, String>
-        
-        //create the url with NSURL
-        let url = NSURL(string: "http://api.samsaodev.com/login/") //change the url
-        
-        //now create the NSMutableRequest object using the url object
-        let request = NSMutableURLRequest(URL: url!)
-        request.HTTPMethod = "POST" //set http method as POST
-        
-        //let err: NSError?
-        request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(parameters, options: []) // pass dictionary to nsdata object and set it as request body
-        
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        var response: NSURLResponse?
+            login.backgroundColor = UIColor.blueColor()
+            //declare parameter as a dictionary which contains string as key and value combination.
+            let parameters = ["username": username.text!, "password": password.text!] as Dictionary<String, String>
+            
+            //create the url with NSURL
+            let url = NSURL(string: "http://api.samsaodev.com/login/") //change the url
+            
+            //now create the NSMutableRequest object using the url object
+            let request = NSMutableURLRequest(URL: url!)
+            request.HTTPMethod = "POST" //set http method as POST
+            
+            //let err: NSError?
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(parameters, options: []) // pass dictionary to nsdata object and set it as request body
+            
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            var response: NSURLResponse?
             do {
                 let dataVal = try NSURLConnection.sendSynchronousRequest(request, returningResponse: &response) as NSData?
-                if let jsonResult = try NSJSONSerialization.JSONObjectWithData(dataVal!, options:[]) as? NSDictionary {
-                    print("\(jsonResult)")
-                }
-                }
                 // look at the response
                 if let httpResponse = response as? NSHTTPURLResponse {
-                    print("HTTP response: \(httpResponse.statusCode)")
-                    print("data \(httpResponse.allHeaderFields)")
-                    print("full Response: \(response)")
+                    if(httpResponse.statusCode == 200){
+                        if let jsonResult = try NSJSONSerialization.JSONObjectWithData(dataVal!, options:[]) as? NSDictionary {
+                            print("\(jsonResult)")
+                            print("Name: \(jsonResult.valueForKey("name")!)")
+                            let username = jsonResult.valueForKey("name")!
+                            let sig = jsonResult.valueForKey("signature")!
+                            _ = jsonResult.valueForKey("photo")!
+                            let nom = UILabel()
+                            nom.text = username as! String
+                            let sign = UILabel()
+                            sign.text = sig as! String
+                            viewMe(nom, signature: sign)
+                        }
+                        
+                    }
+                    else{
+                        print("non-200 code")
+                    }
+                    //print("HTTP response: \(httpResponse.statusCode)")
+                    //print("data \(httpResponse.allHeaderFields)")
+                    //print("full Response: \(response)")
                 } else {
                     print("No HTTP response")
+                    return
                 }
             } catch (let e) {
                 print(e)
             }
+        }catch{
+            
+        }
+        
         
     }
     
+    func viewMe(name: UILabel, signature: UILabel) {
+        backgroundColor = UIColor.redColor()
+        //sv(
+           // username.removeFromSuperview(),
+            //password.removeFromSuperview()
+       // )
+        layout(
+            100,
+            |name ~ 80,
+            8,
+            |signature ~ 80,
+            "",
+            0
+        )
+        
+        
+    }
     
     
     func loginTapped() {
@@ -117,10 +153,10 @@ class SSOLoginView: UIView {
         
         
     }
-
-
-
-
+    
+    
+    
+    
 }
 
 
